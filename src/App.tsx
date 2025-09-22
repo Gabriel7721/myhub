f0d199b666ed443f4edeb7b2c9122dba3892b608
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2025-09-22 18:53:50 +0700

    2nd commit: Installation Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button, HStack } from "@chakra-ui/react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <HStack>
+      <Button>Click Me!</Button>
+      <Button>Click Me!</Button>
+    </HStack>
+  );
 };
 
 export default App;
