902967d73af273e81a0bb895da33d34548ae2190
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2025-09-22 19:20:28 +0700

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <HStack>
-      <Button>Click Me!</Button>
-      <Button>Click Me!</Button>
-    </HStack>
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}>
+      <GridItem area={"nav"} bg={"coral"}>
+        Navigation Bar
+      </GridItem>
+
+      <Show when={"lg"}>
+        <GridItem
+          area={"aside"}
+          bg={"gold"}
+          display={{ base: "none", lg: "block" }}>
+          Left Side Bar
+        </GridItem>
+      </Show>
+
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Main Items
+      </GridItem>
+    </Grid>
   );
 };
 
